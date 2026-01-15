9dcfd8ff4da20f57decb9c5d3003316677ef883f
Campbell <115045281+Lebmac@users.noreply.github.com>
2026-01-16 00:35:51 +1300

    Create 2026-01-15_arraySwap.js

---
+++ 2026-01-15_arraySwap.js
@@ -0,0 +1,14 @@
+/*
+=== Array Swap ===
+=== Source ===
+https://www.freecodecamp.org/learn/daily-coding-challenge/2026-01-15
+
+Given an array with two values, return an array with the values swapped.
+
+For example, given ["A", "B"] return ["B", "A"].
+*/
+
+function arraySwap(arr) {
+
+  return arr.reverse();
+}
