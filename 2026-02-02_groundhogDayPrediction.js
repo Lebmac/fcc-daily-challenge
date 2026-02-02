2102324aea63a0cf8110178596a20460405dce45
Campbell <115045281+Lebmac@users.noreply.github.com>
2026-02-02 20:28:04 +1300

    Create 2026-02-02_groundhogDayPrediction.js

---
+++ 2026-02-02_groundhogDayPrediction.js
@@ -0,0 +1,28 @@
+/* 
+=== Groundhog Day ===
+=== Source ===
+https://www.freecodecamp.org/learn/daily-coding-challenge/2026-02-02
+
+Today is Groundhog Day, in which a groundhog predicts the weather based on whether or not it sees its shadow.
+
+=== Input / Output ===
+Given a value representing the groundhog's appearance, return the correct prediction:
+
+If the given value is the boolean true (the groundhog saw its shadow), return "Looks like we'll have six more weeks of winter.".
+If the value is the boolean false (the groundhog did not see its shadow), return "It's going to be an early spring.".
+If the value is anything else (the groundhog did not show up), return "No prediction this year.". 
+*/
+
+function groundhogDayPrediction(appearance) {
+  const msg = [
+    "It's going to be an early spring.",
+    "Looks like we'll have six more weeks of winter.",
+    "No prediction this year."
+  ];
+
+  let select = 2;
+  if (typeof appearance === "boolean")
+    select = Number(appearance);
+    
+  return msg[select];
+}
