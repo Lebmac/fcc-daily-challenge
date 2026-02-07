9eb58ca244884568779bd661551dd6da2ff004f7
Campbell <115045281+Lebmac@users.noreply.github.com>
2026-02-07 19:51:40 +1300

    Create 2026-02-07_ getLandingStance.js

---
+++ 2026-02-07_ getLandingStance.js
@@ -0,0 +1,26 @@
+/* 
+=== 2026 Winter Games Day 2: Snowboarding ===
+=== Source ===
+https://www.freecodecamp.org/learn/daily-coding-challenge/2026-02-07
+
+Given a snowboarder's starting stance and a rotation in degrees, determine their landing stance.
+
+A snowboarder's stance is either "Regular" or "Goofy".
+Trick rotations are multiples of 90 degrees. Positive indicates clockwise rotation, and negative indicate counter-clockwise rotation.
+The landing stance flips every 180 degrees of rotation.
+For example, given "Regular" and 90, return "Regular". Given "Regular" and 180 degrees, return "Goofy". 
+*/
+
+function getLandingStance(startStance, rotation) {
+  const switchStance = {
+    Regular: "Goofy",
+    Goofy: "Regular",
+  };
+  
+  const hTurns = Math.abs(rotation) / 180;
+  const toggle = Boolean(Math.floor(hTurns) % 2); 
+  
+  return toggle ?
+         switchStance[startStance] :
+         startStance;
+}
