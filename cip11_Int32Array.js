71f9504deb6bd5703347ec09e86f9ba87bc45ae6
Campbell <115045281+Lebmac@users.noreply.github.com>
2026-02-12 09:02:41 +1300

    Create cip11_Int32Array.js

---
+++ cip11_Int32Array.js
@@ -0,0 +1,7 @@
+/* 
+=== Source ===
+https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/typed-arrays 
+*/
+
+var buffer = new ArrayBuffer(64);
+var i32View = new Int32Array(buffer);
